bbf615ff59d9d98c25efc5b66f783ed0380789fd
zhiyi <zy@flizi.cn>
2020-08-28 10:46:02 +0800

    init project

---
+++ upms/src/main/java/cn/flizi/cloud/upms/UpmsApplication.java
@@ -0,0 +1,13 @@
+package cn.flizi.cloud.upms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpmsApplication.class, args);
+	}
+
+}
